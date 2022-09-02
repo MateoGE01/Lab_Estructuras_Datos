b1e1724fc739aa9c7961950f03b9295fbf0c38f5
MateoGE01 <mateoguerrero@uninorte.edu.co>
2022-09-02 12:28:27 -0500

    Comparación lenguajes

--- Scripts/Lecturajava.java
+++ Scripts/Lecturajava.java
@@ -3,7 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader;
 
-class readcsv {
+class LecturaCSV {
     private static String[] datos;
 
     public static void main(String[] args) {
@@ -11,8 +11,8 @@ class readcsv {
         String sample = ",";
         String mystring;
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("Scripts\\SalesRecords.csv"));
-            while ((mystring = reader.readLine()) != null) // Reads a line of text
+            BufferedReader lectura = new BufferedReader(new FileReader("Scripts\\SalesRecords.csv"));
+            while ((mystring = lectura.readLine()) != null) // Reads a line of text
             {
                 datos = mystring.split(sample);
                 System.out.println("['" + datos[0] + "', '" + datos[1] + "', '" + datos[2] + "', '" + datos[3] + "', '"
@@ -20,13 +20,12 @@ class readcsv {
                         + "', '" + datos[9] + "', '" + datos[10] + "', '" + datos[11] + "', '" + datos[12] + "', '"
                         + datos[13] + "']");
             }
-            reader.close();
+            lectura.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
         long end = System.nanoTime();
         long tiempo = end - start;
-        System.out.println(tiempo);
         float tiempoSegundos = (float) tiempo / 1000000000;
         System.out.println(tiempoSegundos+" segundos");
     }
